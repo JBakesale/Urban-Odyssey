b2bbf44282a3dc780013201459b0ec422b5f952c
ahmaddaadaa <ahmadd@uvic.ca>
2023-09-02 00:13:32 -0700

    Main parent component for the app; it renders the HomeRoute component

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+import HomeRoute from './routes/HomeRoute';
+
+function App() {
+  return (
+    <HomeRoute/>
+    
+  );
+}
+
+export default App;

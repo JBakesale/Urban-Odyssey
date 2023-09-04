89cccda7abf7d5327c18fac9ad027ab78782dbf6
Joshua Baker <joshuadfbaker@gmail.com>
2023-09-03 17:56:08 -0700

    format cors.rb

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -11,7 +11,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      # credentials: true # => if we need to support cookies or authentication
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      # credentials: true # if we need to support cookies or authentication
   end
 end
+

72d07d1fb572826e19d8172d0ea959efb06aec8b
Joshua Baker <joshuadfbaker@gmail.com>
2023-09-06 18:30:19 -0700

    AdventurePage with axios fetch and Start button

---
+++ client/src/components/AdventurePage.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import "../styles/AdventurePage.scss";
+
+function AdventurePage() {
+  const { id } = useParams();
+  const [adventure, setAdventure] = useState(null);
+
+  useEffect(() => {
+    console.log(`Fetching Axios request to /adventures/${id}`);
+    axios
+      .get(`/adventures/${id}`)
+      .then((response) => {
+        console.log("Adv Data:", response.data);
+        setAdventure(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
+
+  if (!adventure) {
+    return <div>Loading Adventure...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{adventure.adventure_name}</h1>
+      <p>Tag: {adventure.adventure_tag}</p>
+      <p>Difficulty: {adventure.difficulty}</p>
+      <button>Start Adventure!</button>
+    </div>
+  );
+}
+
+export default AdventurePage;

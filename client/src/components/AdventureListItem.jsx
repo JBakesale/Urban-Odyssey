98eaf21c65749aa84153f6d095e30265935ffd33
Lucas Molina <molinag.lucas@gmail.com>
2023-09-07 20:46:09 -0700

    Adjust home page styling to google maps component

--- client/src/components/AdventureListItem.jsx
+++ client/src/components/AdventureListItem.jsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React from 'react';
+import '../styles/AdventureListItem.scss'
 import { Link } from "react-router-dom";
-import "../styles/AdventureListItem.scss";
 
 function AdventureListItem(props) {
   const { adventure } = props;
 
   return (
-
+    <div className="adventure-container" key={adventure.id}>
     <Link to={`/adventure/${adventure.id}`}>
-      <div className="adventure-container" key={adventure.id}>
-        <div className="image-container">
-          <img src={adventure.adventure_image} alt={`Adventure ${adventure.adventure_name}`} />
-        </div>
-        <div className="text-container">
-          <p className="difficulty">Difficulty: {adventure.difficulty}</p>
-        </div>
+      <div className="image-container">
+        <img src={adventure.adventure_image} alt={`Adventure ${adventure.adventure_name}`} />
+      </div>
+      <div className="text-container">
+        <h2 className="description">{adventure.adventure_name}</h2>
+        <p className="difficulty">Difficulty: {adventure.difficulty}</p>
       </div>
     </Link>
+    </div>
   );
 }
 

95f955853776c31d1fa96c7a770f0972ed7a39d0
Lucas Molina <molinag.lucas@gmail.com>
2023-09-07 17:23:36 -0700

    Add hero, quest board, and adventure images - add adventure_description - fix adventure_list_item data fetch - update database with complete adventure and location data

--- client/src/components/AdventureListItem.jsx
+++ client/src/components/AdventureListItem.jsx
@@ -7,10 +7,10 @@ function AdventureListItem(props) {
   return (
     <div className="adventure-container" key={adventure.id}>
       <div className="image-container">
-        <img src={adventure.imageUrl} alt={`Adventure ${adventure.adventure_name}`} />
+        <img src={adventure.adventure_image} alt={`Adventure ${adventure.adventure_name}`} />
       </div>
       <div className="text-container">
-        <h2 className="description">{adventure.description}</h2>
+        <h2 className="description">{adventure.adventure_name}</h2>
         <p className="difficulty">Difficulty: {adventure.difficulty}</p>
       </div>
     </div>

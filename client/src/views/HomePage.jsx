05899074d4f82141c4f1c324c432efa18eb0c71a
Joshua Baker <joshuadfbaker@gmail.com>
2023-10-30 16:00:43 -0700

    renders maps on homepage, fetches AdventureId and locations context

--- client/src/views/HomePage.jsx
+++ client/src/views/HomePage.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BackgroundContainer from "../components/BackgroundContainer";
 import AdventuresList from "../components/AdventuresList";
 import '../styles/HomePage.scss'
+import MapsApi from "../components/MapsApi";
+import axios from "axios";
+import { useAdventureId } from "../providers/AdventureProvider";
+import { LocationsContext } from "../providers/LocationsProvider";
 
 const HomePage = () => {
+  const { adventureId, setAdventureId } = useAdventureId();
+  const { locations, setLocations } = useContext(LocationsContext);
 
+  useEffect(() => {
+    setAdventureId(null);
+    axios.get('/locations').then(response => {
+      console.log('fetching locations data', response.data)
+      setLocations(response.data);
+    })
+  }, [setAdventureId]);
  
   return (
     <>
       <BackgroundContainer/>
       <AdventuresList/>
+      <MapsApi/>
     </>
   );
 };

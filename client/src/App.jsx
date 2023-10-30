f3a2f8cfaa94357bbbe3df13549a54ce15de46b4
Joshua Baker <joshuadfbaker@gmail.com>
2023-10-30 16:27:42 -0700

    wraps router component tree with providers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './views/HomePage';
-import AboutPage from './views/AboutPage';
-import ContactUsPage from './views/ContactUsPage';
-import UserDetailsPage from './views/UserDetailsPage';
-import SignUp from './views/SignUp';
-import Login from './views/Login';
-import AdventurePage from './components/AdventurePage';
-import ResponsiveAppBar from './components/AppBar';
-import { AdventureProvider } from './providers/AdventureProvider';
-import { UserLocationProvider } from './providers/UserLocationProvider';
-import { LocationsProvider } from './providers/LocationsProvider';
-
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./views/HomePage";
+import AboutPage from "./views/AboutPage";
+import ContactUsPage from "./views/ContactUsPage";
+import UserDetailsPage from "./views/UserDetailsPage";
+import SignUp from "./views/SignUp";
+import Login from "./views/Login";
+import AdventurePage from "./components/AdventurePage";
+import ResponsiveAppBar from "./components/AppBar";
+import { AdventureProvider } from "./providers/AdventureProvider";
+import { UserLocationProvider } from "./providers/UserLocationProvider";
+import { LocationsProvider } from "./providers/LocationsProvider";
 
 function App() {
   return (
-        <Router>
-          <ResponsiveAppBar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/profile" element={<UserDetailsPage />} />
-            <Route path="/about-us" element={<AboutPage />} />
-            <Route path="/contact" element={<ContactUsPage />} />
-            <Route path="/adventure/:id" element={<AdventurePage />} />
-          </Routes>
-        </Router>
-
+    <UserLocationProvider>
+      <AdventurePage>
+        <LocationsProvider>
+          <Router>
+            <ResponsiveAppBar />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/profile" element={<UserDetailsPage />} />
+              <Route path="/about-us" element={<AboutPage />} />
+              <Route path="/contact" element={<ContactUsPage />} />
+              <Route path="/adventure/:id" element={<AdventurePage />} />
+            </Routes>
+          </Router>
+        </LocationsProvider>
+      </AdventurePage>
+    </UserLocationProvider>
   );
 }
 
